8feaf585e6703c8471c46df20f4e163dac36a95b
Yazmin Caamal <yazcaamalfranco@gmail.com>
2025-10-15 19:39:37 -0600

    Prueba Mockito en DependencyTest

--- src/main/java/com/calidad/unittest/dependency/SubDependency.java
+++ src/main/java/com/calidad/unittest/dependency/SubDependency.java
@@ -5,4 +5,11 @@ public class SubDependency {
 	public String getClassName() {
 		return this.getClass().getSimpleName();
 	}
+	public int addTwo(int i) {
+		return i + 2;
+	}
+
+	public Integer addTwo(Integer i) {
+		return i + 2;
+	}
 }
